a85a523f84eccc543bd956cb5908328dbe0498e7
Dilyara Shaydullina <dilyara.shhh@gmail.com>
2024-11-25 04:32:42 +0700

    дто для адресов

---
+++ socials/DBContext/DTO/GAR/LevelDTO.cs
@@ -0,0 +1,10 @@
+namespace socials.DBContext.DTO.GAR;
+
+public class LevelDTO
+{
+    public int Id { get; set; }
+    
+    public string levelName { get; set; }
+    
+    public string levelTranslateName { get; set; }
+}
